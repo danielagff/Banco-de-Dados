fa49132091c9d6cef2e6cf8af000724f22a93f92
DanielFilho2000 <daniel.agff@gmail.com>
2021-01-26 15:47:15 -0300

    CRUD farmacia

---
+++ src/main/java/com/farmacia/farmacia/model/Produtos.java
@@ -0,0 +1,61 @@
+package com.farmacia.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "produtos")
+public class Produtos 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	@Size(min = 1 , max = 100)
+	private String nome;
+	
+	@NotNull
+	@Size(min = 1 , max = 500)
+	private String tipo;
+	
+	@NotNull
+	private int quantidade;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+}
